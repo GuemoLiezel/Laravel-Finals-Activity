0200a4204c66f063e91da3f9caa28a7dc4d8625c
Liezel Guemo <101855973+GuemoLiezel@users.noreply.github.com>
2022-06-13 20:40:48 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,15 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/', function()
+{
+    return View::make('pages.home');
+
+});
+
+Route::get('/contact', function()
+{
+    return View::make('pages.contact');
+
+});
