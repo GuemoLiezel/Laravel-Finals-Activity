dece99c62404ff6aa4a407b42742f656d1ce00bb
Liezel Guemo <101855973+GuemoLiezel@users.noreply.github.com>
2022-06-13 21:15:19 -0800

    Update header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,1 +1,16 @@
+<div class="navbar">
+    <div class="navbar-inner">
 
+        <a id="logo" style="display:none">Liezel Guemo</a>
+    
+        <ul class="nav">
+
+        <li><a href="#section1">Home</a></li>
+        <li><a href="#section2">CS202</a></li>
+        <li><a href="#section3">Laravel</a></li>
+        <li><a href="#section4">About</a></li>
+        <li><a href="#section5">Contact</a></li>
+
+        </ul>
+    </div>
+</div>
